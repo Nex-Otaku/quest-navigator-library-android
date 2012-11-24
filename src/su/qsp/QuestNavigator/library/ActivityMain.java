5a6b41829a36e64d3bb59566b62fef532247446b
Nex Otaku <nex@otaku.ru>
2012-11-24 17:40:46 +0400

    добавил сплэш-скрин

--- src/su/qsp/QuestNavigator/library/ActivityMain.java
+++ src/su/qsp/QuestNavigator/library/ActivityMain.java
@@ -13,11 +13,12 @@ public class ActivityMain extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
         
-        super.loadUrl("file:///android_asset/www/standalone_content/gamePG-android.html");
+        super.loadUrl("file:///android_asset/www/standalone_content/gamePG-android.html", 60000);
     }
 }
